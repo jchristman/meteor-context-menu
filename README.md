2419f0323abfbf4c8fdb3e447379774d0e723a63
Josh <c13joshua.christman@gmail.com>
2014-11-21 10:56:14 -0500

    - Dynamic menus works as expected. Also, added optional structure to   allow a single DOM element to be inserted for multiple elements context menus

--- README.md
+++ README.md
@@ -14,7 +14,57 @@ Defining the menu
 Define the menu as an object: this is a natural way of expressing the context menu. You can define an object with roughly this structure:
 
 ```
-test_menu = [ 
+test_menu = {
+    id: 'TEST-MENU',
+    data: [
+        {
+            header: 'Example'
+        },
+        {
+            icon: 'glyphicon-plus',
+            text: 'Create',
+            action: function(e, selector) { alert('Create clicked on ' + selector.prop("tagName")); }
+        },
+        {
+            icon: 'glyphicon-edit',
+            text: 'Edit',
+            action: function(e, selector) { alert('Edit clicked on ' + selector.prop("tagName")); }
+        },
+        {
+            icon: 'glyphicon-list-alt',
+            text: 'View Data As:',
+            subMenu : [
+            {
+                text: 'Text',
+                action: function(e, selector) { alert('Text clicked on ' + selector.prop("tagName")); }
+            },
+            {
+                text: 'Image',
+                subMenu: [
+                    {
+                        menu_item_src : exampleMenuItemSource
+                    }
+                ]
+            }
+            ]
+        },
+        {
+            divider: true
+        },
+        {
+            header: 'Another Example'
+        },
+        {
+            icon: 'glyphicon-trash',
+            text: 'Delete',
+            action: function(e, selector) { alert('Delete clicked on ' + selector.prop("tagName")); }
+        }
+    ]
+};
+```
+
+```
+test_menu2 = [
     {
         header: 'Example'
     },
@@ -27,44 +77,6 @@ test_menu = [
         icon: 'glyphicon-edit',
         text: 'Edit',
         action: function(e, selector) { alert('Edit clicked on ' + selector.prop("tagName")); }
-    },
-    {
-        icon: 'glyphicon-list-alt',
-        text: 'View Data As:',
-        subMenu : [ 
-        {   
-            text: 'Text',
-            action: function(e, selector) { alert('Text clicked on ' + selector.prop("tagName")); }
-        },  
-        {   
-            text: 'Image',
-            subMenu: [
-            {   
-                text: 'PNG',
-                action: function(e, selector) { alert('PNG clicked on ' + selector.prop("tagName")); }
-            },  
-            {   
-                text: 'JPEG',
-                action: function(e, selector) { alert('JPEG clicked on ' + selector.prop("tagName")); }
-            },  
-            {   
-                text: 'GIF',
-                action: function(e, selector) { alert('GIF clicked on ' + selector.prop("tagName")); }
-            }   
-            ]   
-        }   
-        ]   
-    },
-    {
-        divider: true
-    },
-    {
-        header: 'Another Example'
-    },
-    {
-        icon: 'glyphicon-trash',
-        text: 'Delete',
-        action: function(e, selector) { alert('Delete clicked on ' + selector.prop("tagName")); }
     }
 ];
 ```
--- example/test.js
+++ example/test.js
@@ -1,58 +1,17 @@
-test_menu = [
-{
-    header: 'Example'
-},
-{
-    icon: 'glyphicon-plus',
-    text: 'Create',
-    action: function(e, selector) { alert('Create clicked on ' + selector.prop("tagName")); }
-},
-{
-    icon: 'glyphicon-edit',
-    text: 'Edit',
-    action: function(e, selector) { alert('Edit clicked on ' + selector.prop("tagName")); }
-},
-{
-    icon: 'glyphicon-list-alt',
-    text: 'View Data As:',
-    subMenu : [
-    {
-        text: 'Text',
-        action: function(e, selector) { alert('Text clicked on ' + selector.prop("tagName")); }
-    },
-    {
-        text: 'Image',
-        subMenu: [
-            {
-                menu_item_src : 'exampleMenuItemSource'
-            }
-        ]
-    }
-    ]
-},
-{
-    divider: true
-},
-{
-    header: 'Another Example'
-},
-{
-    icon: 'glyphicon-trash',
-    text: 'Delete',
-    action: function(e, selector) { alert('Delete clicked on ' + selector.prop("tagName")); }
-}
-];
-
-exampleMenuItemSource = function(selector) {
+exampleMenuItemSource = function (selector) {
     if ($(selector).attr('id') == 'PNG_JPG') {
         return [
                 {
-                    text: 'PNG',
-                    action: function(e, selector) { alert('Text clicked on ' + selector.prop("tagName")); }
+                    header: 'Example Dynamic'
                 },
                 {
+                    text: 'PNG',
+                    action: function(e, selector) { alert('PNG clicked on ' + selector.prop("tagName")); }
+                },
+                {   divider: true   },
+                {
                     text: 'JPG',
-                    action: function(e, selector) { alert('Text clicked on ' + selector.prop("tagName")); }
+                    action: function(e, selector) { alert('JPG clicked on ' + selector.prop("tagName")); }
                 }
             ]
     } else {
@@ -64,3 +23,67 @@ exampleMenuItemSource = function(selector) {
             ]
     }
 }
+
+test_menu = {
+    id: 'TEST-MENU',
+    data: [
+        {
+            header: 'Example'
+        },
+        {
+            icon: 'glyphicon-plus',
+            text: 'Create',
+            action: function(e, selector) { alert('Create clicked on ' + selector.prop("tagName")); }
+        },
+        {
+            icon: 'glyphicon-edit',
+            text: 'Edit',
+            action: function(e, selector) { alert('Edit clicked on ' + selector.prop("tagName")); }
+        },
+        {
+            icon: 'glyphicon-list-alt',
+            text: 'View Data As:',
+            subMenu : [
+            {
+                text: 'Text',
+                action: function(e, selector) { alert('Text clicked on ' + selector.prop("tagName")); }
+            },
+            {
+                text: 'Image',
+                subMenu: [
+                    {
+                        menu_item_src : exampleMenuItemSource
+                    }
+                ]
+            }
+            ]
+        },
+        {
+            divider: true
+        },
+        {
+            header: 'Another Example'
+        },
+        {
+            icon: 'glyphicon-trash',
+            text: 'Delete',
+            action: function(e, selector) { alert('Delete clicked on ' + selector.prop("tagName")); }
+        }
+    ]
+};
+
+test_menu2 = [
+    {
+        header: 'Example'
+    },
+    {
+        icon: 'glyphicon-plus',
+        text: 'Create',
+        action: function(e, selector) { alert('Create clicked on ' + selector.prop("tagName")); }
+    },
+    {
+        icon: 'glyphicon-edit',
+        text: 'Edit',
+        action: function(e, selector) { alert('Edit clicked on ' + selector.prop("tagName")); }
+    }
+];
