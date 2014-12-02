bb9485cdacd451531a27ff4ee777b91b8ca84085
Josh <c13joshua.christman@gmail.com>
2014-12-02 06:36:08 -0500

    - Fixed issue with *nix firefox context menus immediately disappearing - Update to bootstrap 3.3.1 to fix issue with glyphicons resource

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'jchristman:context-menu',
     summary: 'Meteor package to wrap a bootstrap context menu',
-    version: '1.1.2_2',
+    version: '1.1.2_3',
     git: 'https://github.com/suntzuII/meteor-bootstrap-context-menu.git'
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
     api.versionsFrom('METEOR@1.0');
 
     api.use('jquery');
-    api.use('mizzao:bootstrap-3@3.2.0'); // Need this for the glyphicons
+    api.use('mizzao:bootstrap-3@3.3.1'); // Need this for the glyphicons
 
     api.addFiles('lib/context.js','client');
     api.addFiles('lib/context.css','client');
