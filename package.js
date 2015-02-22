2794bf7cd006c007045dbc463c1016a49b116c7b
Josh <c13joshua.christman@gmail.com>
2015-02-22 10:25:16 -0500

    - Version bumps and new repo location

--- package.js
+++ package.js
@@ -2,14 +2,14 @@ Package.describe({
     name: 'jchristman:context-menu',
     summary: 'Meteor package to wrap a bootstrap context menu',
     version: '1.1.4',
-    git: 'https://github.com/suntzuII/meteor-bootstrap-context-menu.git'
+    git: 'https://github.com/jchristman/meteor-bootstrap-context-menu.git'
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('METEOR@1.0');
 
     api.use('jquery');
-    api.use('twbs:bootstrap@3.3.1_2'); // Need this for the glyphicons
+    api.use('twbs:bootstrap@3.3.2'); // Need this for the glyphicons
 
     api.addFiles('lib/context.js','client');
     api.addFiles('lib/context.css','client');
